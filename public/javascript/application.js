c6a3249bd0fa9ef01320777ed86e8266c991c123
ctrlshft <julian.rogochevsky@gmail.com>
2015-12-09 02:04:07 +0000

    sentiment bar working

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,18 @@
 
 
 $(document).ready(function() {
-   $('.tooltip').tooltipster();
+  $('.tooltip').tooltipster({
+    animation: 'fade',
+    delay: 20,
+    theme: 'tooltipster-noir',
+    touchDevices: false,
+    trigger: 'hover'
+  });
+
+  $(".article_dot").on('click', function(){
+     var url = $(this).data().url
+     window.open(url, '_blank');   
+  });
 
   
 });
