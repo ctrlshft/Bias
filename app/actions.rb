501c109b6647eab2730a2faefcbb570bce0ed4c4
ctrlshft <julian.rogochevsky@gmail.com>
2015-12-18 18:30:58 +0000

    V1 Final commit

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,7 @@
 BingSearch.account_key = 'hqXahx+6k2dIwnykE26HCrEItoUXM8JmiVB1QBocMhA'
 require_relative'../cache/cache.rb'
-# require_relative'../cache/cache.rb'
+
+# bc9d67cc85047b79b30187500483a6212581839d
 
 get '/' do
 
@@ -9,19 +10,21 @@ get '/' do
   @avg_score_arr = []
   media = params[:media]
 
+  @conn = Faraday.new(headers: {accept_encoding: 'none'}, url: 'https://gateway-a.watsonplatform.net') do |faraday|
+    # faraday.request  :url_encoded             # form-encode POST params
+    faraday.response :logger                  # log requests to STDOUT
+    faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    faraday.use FaradayMiddleware::ParseJson, :content_type => /\bjson$/
+  end
+
   if search_topic
 
-    @conn = Faraday.new(headers: {accept_encoding: 'none'}, url: 'https://gateway-a.watsonplatform.net') do |faraday|
-      # faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-      faraday.use FaradayMiddleware::ParseJson, :content_type => /\bjson$/
-    end
+
 
   #-------------get general sentiment of article-------#
 
     def analyze_url_non_targeted (url)
-      @conn.get "/calls/url/URLGetTextSentiment?apikey=bc9d67cc85047b79b30187500483a6212581839d&url=#{url}&outputMode=json&showSourceText=1&sourceText=cleaned"
+      @conn.get "/calls/url/URLGetTextSentiment?apikey=4d314350027a4905e524e783e548a4e90a04c813&url=#{url}&outputMode=json&showSourceText=1&sourceText=cleaned"
     end
 
   #----------extract seniment--------#
@@ -37,73 +40,72 @@ get '/' do
 
 
 
-    if search_topic == 'syria'
+    if search_topic == 'syria' && media == "ALL"
       @articles = @@array_response_syria
-    elsif search_topic == 'donald trump'
+    elsif search_topic == 'linkedin' && media == "ALL"
+      @articles = @@array_response_linkedin
+    elsif search_topic == 'donald trump' && media == "ALL"
       @articles = @@array_response_donald_trump
+    elsif search_topic == 'drones' && media == "ALL"
+      @articles = @@array_response_drones
     else
     # ---- search bing with topic and return general selection-------- #
-    if media == "ALL"
-      search_response = BingSearch.news("#{search_topic}" , limit: 10, sort:"relevance")
-      # --------- parse responses ------- #
-      search_response.each do |result| 
-        article = {
-          title: result.title,
-          url: result.url,
-          source: result.source
-        }
-        @articles.push(article)
+      if media == "ALL"
+        search_response = BingSearch.news("#{search_topic}" , limit: 10, sort:"relevance")
+        # --------- parse responses ------- #
+        search_response.each do |result| 
+          article = {
+            title: result.title,
+            url: result.url,
+            source: result.source
+          }
+          @articles.push(article)
+        end
+     # ---- search google with topic and return media specific selection-------- #   
+      else
+        search_response = RubyWebSearch::Google.search(:query => "#{search_topic} :site #{media}", :size => 5)
+      # ----- parse responses ----- #
+        search_response.results.each do |result| 
+          article = {
+            title: result[:title],
+            url: result[:url],
+            source: result[:domain]
+          }
+          @articles.push(article)
+        end
       end
-   # ---- search google with topic and return media specific selection-------- #   
-    else
-      search_response = RubyWebSearch::Google.search(:query => "#{search_topic} :site #{media}", :size => 5)
-    # ----- parse responses ----- #
-      search_response.results.each do |result| 
-        article = {
-          title: result[:title],
-          url: result[:url],
-          source: result[:domain]
-        }
-        @articles.push(article)
+
+    # ---- send to alchemy for analysis sentiment extraction ----#
+      @articles.each_with_index do |article, article_index|
+        response = analyze_url_non_targeted(article[:url])
+        extract_sentiment(response, article_index)
       end
+
+    # ------ average sentiment score of all articles ----- #
+      @average_sentiment_score = @avg_score_arr.sum / @avg_score_arr.size.to_f
+
+
+    # ----- average sentiment type of all articles ----- #
+      if @average_sentiment_score > 0
+        @average_sentiment_type = 'positive'
+      else
+        @average_sentiment_type = 'negative'
+      end
+
+    # # --- sort by sentiment scores and find sentiment range (will plot by relative sentiment----#
+      @articles = @articles.reject {|article| (article[:score] == nil)||(article[:score] == 0)}
+      @articles = @articles.sort_by { |hsh| hsh[:score] }
+      
+
+      @articles.each do |article|
+       article[:normalized_score_absolute] = (article[:score] + 1) * 50
+      end
+
+      @articles.each do |article|
+       article[:normalized_score_relative] = (( article[:score] - @articles[0][:score] ) / ( @articles[-1][:score] - @articles[0][:score])) * 90
+      end
+    # File.write('cache/cache.rb', @articles, mode: 'a')
     end
-
-  # ---- send to alchemy for analysis sentiment extraction ----#
-    @articles.each_with_index do |article, article_index|
-      response = analyze_url_non_targeted(article[:url])
-      extract_sentiment(response, article_index)
-    end
-
-  # ------ average sentiment score of all articles ----- #
-    @average_sentiment_score = @avg_score_arr.sum / @avg_score_arr.size.to_f
-
-
-  # ----- average sentiment type of all articles ----- #
-    if @average_sentiment_score > 0
-      @average_sentiment_type = 'positive'
-    else
-      @average_sentiment_type = 'negative'
-    end
-
-  # # --- sort by sentiment scores and find sentiment range (will plot by relative sentiment----#
-    @articles = @articles.reject {|article| (article[:score] == nil)||(article[:score] == 0)}
-    @articles = @articles.sort_by { |hsh| hsh[:score] }
-    
-
-    @articles.each do |article|
-     article[:normalized_score_absolute] = (article[:score] + 1) * 50
-    end
-
-    @articles.each do |article|
-     article[:normalized_score_relative] = (( article[:score] - @articles[0][:score] ) / ( @articles[-1][:score] - @articles[0][:score])) * 90
-    end
-  # File.write('cache/cache.rb', @articles, mode: 'a')
-  # search_topic_underscore = search_topic.gsub(' ','-')
-  # topic_array.push(search_topic_underscore)
   end
-
-  end
-
-
   erb :index
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -33,12 +33,18 @@ $(document).ready(function() {
     }
   });
 
+  $('#title').removeClass('rotate');
+  
+  $('#submit_button').on('click', function(){
+    $('#title').addClass('rotate');
+  });
+
+
+
+
 
   $(".article_dot").on('click', function(event){
     event.stopPropagation();
-
-    console.log("nav dot clicked", event);
-
     var url = $(this).data().url;
     var content = $(this).data().content;
     var score = $(this).data().srel;
@@ -46,17 +52,21 @@ $(document).ready(function() {
 
     if (score < 50) { 
       // $("#negative-article").empty().append("<p> <a href=" + url + "/a>" + Go to full article + "</p>"); 
-      $("#negative-article").css('visibility', 'visible');
+      
+      $("#negative-article").css('visibility','visible');
       $("#negative-article").css('border-color', 'hsl(' + score + ',30%, 50%)'); 
       $("#negative-article").empty().append("<p> <a target='_blank' href=" + url + "/a>" + title + "</p>"); 
       $("#negative-article").append("<p>" + content + "</p>"); 
+    
 
     }
     else {
-      $("#positive-article").css('visibility', 'visible');
+      
+      $("#positive-article").css('visibility','visible');
       $("#positive-article").css('border-color', 'hsl(' + score + ',30%, 50%)'); 
       $("#positive-article").empty().append("<p> <a target='_blank' href=" + url + "/a>" + title + "</p>"); 
       $("#positive-article").append("<p>" + content + "</p>"); 
+
     }
   });
 
